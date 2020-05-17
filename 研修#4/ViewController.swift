4361e8a2e4e2813a8423725b5b411a2a413568e1
pluiel <pluiel@outlook.com>
2020-05-16 22:10:56 -0700

    Initial Commit

---
+++ 研修#4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  研修#4
+//
+//  Created by Yuka Uematsu on 2020/05/16.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
